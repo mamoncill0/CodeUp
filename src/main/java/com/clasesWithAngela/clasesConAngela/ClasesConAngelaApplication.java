52421348eefe3fc018db18ea4606b6dc7727142e
MiPrueba <michaelfrancofalla@gmail.com>
2025-11-07 09:27:50 -0500

    proyecto

---
+++ src/main/java/com/clasesWithAngela/clasesConAngela/ClasesConAngelaApplication.java
@@ -0,0 +1,13 @@
+package com.clasesWithAngela.clasesConAngela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasesConAngelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasesConAngelaApplication.class, args);
+	}
+
+}
